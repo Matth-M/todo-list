5aff275042c8e283f11b1a2c8054141b697aea52
Matth-M <matthieu.matsima@gmail.com>
2022-12-18 23:43:24 +0100

    Add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,12 @@ module.exports = {
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader'],
 			},
-		]
+
+			{
+				test: /\.s[ac]ss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
 	},
 	optimization: {
 		runtimeChunk: 'single',
