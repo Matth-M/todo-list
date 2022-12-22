97a49a376912dd34095649b2b8992142e8b03181
Matth-M <matthieu.matsima@gmail.com>
2022-12-22 17:42:30 +0100

    Call proper function in index.ts to display the app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import {displayList, displayPageStructure} from './display'
+import {displayPageStructure} from './display'
 import {Todo} from './todo'
 import {TodoList} from './todoList';
 
 
-// let t1 = Todo();
+// Dummy item
 let t1 = Todo("Train", "Rentrer", "20/12/22", 1);
+let t2 = Todo("Train", "Rentrer", "20/12/22", 1);
 t1.setDueDate("20/12/2022");
 t1.setTitle("Prendre le train");
 t1.setDescription("Faut bien rentrer chez soi");
 t1.setPriority(2);
 
-const main = TodoList("main");
-main.addTodo(t1);
+const mainList = TodoList("main");
+mainList.addTodo(t1);
+mainList.addTodo(t2);
 
-displayPageStructure();
-displayList(main);
+displayPageStructure(mainList);
