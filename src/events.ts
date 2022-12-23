ccef56827955c1a60cd4b6c1c786870e26da6b7d
Matth-M <matthieu.matsima@gmail.com>
2022-12-23 18:37:20 +0100

    Add a handler to the add Todo button

--- src/events.ts
+++ src/events.ts
@@ -1,13 +1,31 @@
 import {TodoList} from './todoList' 
+import { Todo } from './todo'
 import {displayApp} from './display'
 
-export function addDeleteBtnHandler(list: TodoList) {
+export function deleteBtnHandler(list: TodoList) {
 	const deleteBtns = document.querySelectorAll('.todo > .delete');
 	deleteBtns.forEach((btn) => {
 		btn.addEventListener('click', () => {
 			const index = Number(btn.getAttribute('index'));
+
 			list.deleteTodo(index);
 			displayApp(list);
 		})
+	});
+}
+
+export function addButtonHandler(list: TodoList){
+	const addBtn = document.querySelector('#addTodoBtn');
+	addBtn.addEventListener('click', () => {
+		const title: HTMLInputElement = document.querySelector('#titleInput');
+		const description: HTMLInputElement = document.querySelector('#descriptionInput');
+		const dueDate: HTMLInputElement = document.querySelector('#dueDateInput');
+		const priority: HTMLInputElement = document.querySelector('#priorityInput');
+
+		console.log(title);
+		const todo = Todo(title.value, description.value, dueDate.value, Number(priority.value));
+
+		list.addTodo(todo);
+		displayApp(list);
 	});
 }
